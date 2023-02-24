bd8d0a2e634a1f32133361ceed1909d42376b34a
RichieRich2020clear <abhijeetkamble9cd@gmail.com>
2023-02-24 10:22:07 +0530

    done

--- controller/PostController.js
+++ controller/PostController.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const { MenuData } = require('../models/postDataModel');
 const { mealscategories } = require('../models/MealsCategories');
 const { CartData } = require('../models/cartmodel');
-
 const authenticate = require('../middleware/Authentication');
 const postrouter = express.Router();
 
